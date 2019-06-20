6de2f2d0aeec1a61ca6bea88e3729df056539924
Siddhant Sinha <35433185+sid573@users.noreply.github.com>
2019-06-21 01:04:52 +0530

    content chaange

--- algo8/matrix.py
+++ algo8/matrix.py
@@ -67,25 +67,9 @@ def opini8():
 def career():
     return render_template('careers.html')
 
-@app.route('/dimension')
-def dimension():
-    return render_template('dimension.html')
-
 @app.route('/team')
 def team():
     return render_template('team.html')
-
-@app.route('/events')
-def events():
-    return render_template('events.html')
-
-@app.route('/events/')
-def event():
-    return redirect("/events", code=302)
-
-@app.route('/dimension/gallery')
-def dim_gallery():
-    return render_template('dim_gallery.html')
 
 
 if __name__ == '__main__':
